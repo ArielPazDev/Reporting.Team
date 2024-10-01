af56af3dce56d4334c0ae3332ead265c3e5c0c0b
ArielPazDev <contacto.arielpaz.dev@gmail.com>
2024-10-01 04:29:09 -0300

    feat (Backend.API.RESTful): add rol controller

---
+++ Backend.API.RESTful/Controllers/RolController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend.API.RESTful.Context;
+using Backend.API.RESTful.Models;
+
+namespace Backend.API.RESTful.Controllers
+{
+    [Route("api/roles")]
+    [ApiController]
+    public class RolController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RolController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Rol
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RolModel>>> GetRoles()
+        {
+            return await _context.Roles.ToListAsync();
+        }
+
+        // GET: api/Rol/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RolModel>> GetRolModel(int id)
+        {
+            var rolModel = await _context.Roles.FindAsync(id);
+
+            if (rolModel == null)
+            {
+                return NotFound();
+            }
+
+            return rolModel;
+        }
+
+        // PUT: api/Rol/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRolModel(int id, RolModel rolModel)
+        {
+            if (id != rolModel.IDRol)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(rolModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RolModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Rol
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<RolModel>> PostRolModel(RolModel rolModel)
+        {
+            _context.Roles.Add(rolModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRolModel", new { id = rolModel.IDRol }, rolModel);
+        }
+
+        // DELETE: api/Rol/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRolModel(int id)
+        {
+            var rolModel = await _context.Roles.FindAsync(id);
+            if (rolModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Roles.Remove(rolModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RolModelExists(int id)
+        {
+            return _context.Roles.Any(e => e.IDRol == id);
+        }
+    }
+}
